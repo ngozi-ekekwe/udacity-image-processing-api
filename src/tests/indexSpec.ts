a9a6f8b0fbdea82f690a08e87fdf2bd4680b4a3d
Ngozi Ekekwe <ngozi-ekekwe>
2021-05-29 20:45:37 +0200

    fix linter

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -18,35 +18,43 @@ describe("Server API", () => {
 });
 
 describe("Resize Controller Middleware", () => {
-  it('should return an error is a parameter is missing', async(done) => {
+  it("should return an error is a parameter is missing", async (done) => {
     const response = await request.get("/api/images");
-    expect(response.text).toBe('Error: Parameter(s) missing..');
+    expect(response.text).toBe("Error: Parameter(s) missing..");
     done();
   });
 
-  it('should return an error message if width is not a number', async(done) => {
-    const response = await request.get("/api/images?filename=danceforme&width=hello&height=400");
-    expect(response.text).toBe('height and width should be numbers');
+  it("should return an error message if width is not a number", async (done) => {
+    const response = await request.get(
+      "/api/images?filename=danceforme&width=hello&height=400"
+    );
+    expect(response.text).toBe("height and width should be numbers");
     done();
   });
 
-  it('should return an error message if height is not a number', async(done) => {
-    const response = await request.get("/api/images?filename=danceforme&width=hello&height=hello");
-    expect(response.text).toBe('height and width should be numbers');
+  it("should return an error message if height is not a number", async (done) => {
+    const response = await request.get(
+      "/api/images?filename=danceforme&width=hello&height=hello"
+    );
+    expect(response.text).toBe("height and width should be numbers");
     done();
   });
 
-  it('should return a status of 400 if an error occured', async(done) => {
-    const response = await request.get("/api/images?filename=danceforme&width=hello&height=hello");
+  it("should return a status of 400 if an error occured", async (done) => {
+    const response = await request.get(
+      "/api/images?filename=danceforme&width=hello&height=hello"
+    );
     expect(response.status).toBe(400);
     done();
   });
 
-  describe('Resize Controller', () => {
-    it('should resize a full image into the public folder', async(done) => {
-      const response = await request.get("/api/images?filename=danceforme&width=400&height=300");
+  describe("Resize Controller", () => {
+    it("should resize a full image into the public folder", async (done) => {
+      const response = await request.get(
+        "/api/images?filename=danceforme&width=400&height=300"
+      );
       expect(response.status).toBe(200);
       done();
-    })
-  })
-})
+    });
+  });
+});
