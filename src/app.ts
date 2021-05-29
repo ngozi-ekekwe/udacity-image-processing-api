aef05f18d325c8c157bbc1099f9d28e7191ee45f
Ngozi Ekekwe <ngozi-ekekwe>
2021-05-29 18:20:48 +0200

    chore:setup eslint

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response} from 'express';
-import path from 'path';
+import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname + "public")));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Image Processing API')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Image Processing API");
+});
 
 export default app;
