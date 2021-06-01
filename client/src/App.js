202dc07b3ee0ce4cbb86a10a2bc3780eb49d0c52
Ngozi Ekekwe <ngozi-ekekwe>
2021-06-01 23:17:43 +0200

    feat: client structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {
+  Container,
+  Menu,
+  Segment,
+  Header,
+  Button,
+  Checkbox,
+  Form,
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
+  const [activeItem, setActiveItem] = useState("thumbnails");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
+  const fetchAllThumbnails = () => {};
+
+  const uploadImages = () => {
+    return (
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
+  };
+
+  const resizeImages = () => {
+    return (
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Menu>
+          <Menu.Item
+            name="thumbnails"
+            active={activeItem === "thumbnails"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name="resizeImages"
+            active={activeItem === "resizeImages"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name="upload"
+            active={activeItem === "upload"}
+            onClick={handleItemClick}
+          />
+        </Menu>
+
+        <Container text>
+          {activeItem === "thumbnails" && <p>Thumnails</p>}
+
+          {activeItem === "resizeImages" && resizeImages()}
+
+          {activeItem === "upload" && uploadImages()}
+        </Container>
+      </div>
     </div>
   );
 }
